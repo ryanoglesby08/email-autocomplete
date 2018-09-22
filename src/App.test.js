eab8649c6cbd81375c4b37deb83ea965e91f35bb
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-09-22 09:05:59 -0500

    use react-testing-library and jest-dom for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+
+import { render } from 'react-testing-library'
+
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const { container } = render(<App />)
+
+  expect(container).toHaveTextContent('Welcome to React')
+})
