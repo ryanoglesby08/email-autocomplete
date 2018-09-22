02d34bb780951ffcd41bc7162ae7ff8fb4153aa4
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-09-22 12:46:39 -0500

    Refactor Composer into sub components to separate network concerns from the form

--- src/Composer.js
+++ src/Composer.js
@@ -4,117 +4,160 @@ import { sendEmail } from './api/emailService'
 
 class Composer extends Component {
   state = {
-    emailSent: false,
-    errorMessage: undefined,
-    clientError: false,
-    serverError: false,
     to: '',
     cc: '',
     subject: '',
     body: '',
   }
 
-  submitDraft = async () => {
-    const { to, cc, subject, body } = this.state
+  reset = () => {
+    this.setState({
+      to: '',
+      cc: '',
+      subject: '',
+      body: '',
+    })
+  }
+
+  render() {
+    return (
+      <SendEmail onSuccess={this.reset}>
+        {({ submitDraft, feedbackMessage }) => {
+          return (
+            <ComposerForm
+              to={this.state.to}
+              cc={this.state.cc}
+              subject={this.state.subject}
+              body={this.state.body}
+              feedbackMessage={feedbackMessage}
+              onSubmit={submitDraft}
+              onFieldChange={(field, e) =>
+                this.setState({ [field]: e.target.value })
+              }
+            />
+          )
+        }}
+      </SendEmail>
+    )
+  }
+}
+
+const FeedbackMessage = ({ success, error }) => {
+  if (success) {
+    return <div>🎉 Email sent successfully!</div>
+  } else {
+    if (error.kind === 'CLIENT_ERROR') {
+      return (
+        <Fragment>
+          <div>
+            🤔 Oops, looks like you made a mistake. Please correct any errors
+            and try again.
+          </div>
+          <div>{error.message}</div>
+        </Fragment>
+      )
+    } else {
+      return (
+        <div>🤭 Sorry about this, we screwed up. Will you try that again?</div>
+      )
+    }
+  }
+}
+
+class SendEmail extends Component {
+  state = {
+    done: false,
+    success: undefined,
+    error: undefined,
+  }
+
+  submitDraft = async ({ to, cc, subject, body }) => {
+    const { onSuccess } = this.props
 
     try {
       await sendEmail({ to, cc: cc.split(','), subject, body })
 
+      onSuccess()
       this.setState({
-        emailSent: true,
-        to: '',
-        cc: '',
-        subject: '',
-        body: '',
+        done: true,
+        success: true,
       })
-    } catch ({ kind, message }) {
-      if (kind === 'CLIENT_ERROR') {
-        this.setState({ clientError: true, errorMessage: message })
-      }
-      if (kind === 'SERVER_ERROR') {
-        this.setState({ serverError: true })
-      }
+    } catch (error) {
+      this.setState({ done: true, success: false, error })
     }
   }
 
   render() {
-    const {
-      emailSent,
-      clientError,
-      serverError,
-      errorMessage,
-      to,
-      cc,
-      subject,
-      body,
-    } = this.state
+    const { children } = this.props
+    const { done, success, error } = this.state
 
-    return (
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-          this.submitDraft()
-        }}
-      >
-        {emailSent && <div>🎉 Email sent successfully!</div>}
-        {serverError && (
-          <div>
-            🤭 Sorry about this, we screwed up. Will you try that again?
-          </div>
-        )}
-        {clientError && (
-          <Fragment>
-            <div>
-              🤔 Oops, looks like you made a mistake. Please correct any errors
-              and try again.
-            </div>
-            <div>{errorMessage}</div>
-          </Fragment>
-        )}
-
-        <div>
-          <label htmlFor="to">To</label>
-          <input
-            id="to"
-            type="text"
-            value={to}
-            onChange={e => this.setState({ to: e.target.value })}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="cc">Cc</label>
-          <input
-            id="cc"
-            type="text"
-            value={cc}
-            onChange={e => this.setState({ cc: e.target.value })}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="subject">Subject</label>
-          <input
-            id="subject"
-            type="text"
-            value={subject}
-            onChange={e => this.setState({ subject: e.target.value })}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="body">Body</label>
-          <textarea
-            id="body"
-            value={body}
-            onChange={e => this.setState({ body: e.target.value })}
-          />
-        </div>
-
-        <button type="submit">Send</button>
-      </form>
-    )
+    return children({
+      submitDraft: this.submitDraft,
+      feedbackMessage: done && (
+        <FeedbackMessage success={success} error={error} />
+      ),
+    })
   }
 }
+
+const ComposerForm = ({
+  to,
+  cc,
+  subject,
+  body,
+  onSubmit,
+  onFieldChange,
+  feedbackMessage,
+}) => (
+  <form
+    onSubmit={e => {
+      e.preventDefault()
+      onSubmit({ to, cc, subject, body })
+    }}
+  >
+    {feedbackMessage}
+
+    <div>
+      <label htmlFor="to">To</label>
+      <input
+        id="to"
+        type="text"
+        value={to}
+        onChange={e => onFieldChange('to', e)}
+      />
+    </div>
+
+    <div>
+      <label htmlFor="cc">Cc</label>
+      <input
+        id="cc"
+        type="text"
+        value={cc}
+        onChange={e => onFieldChange('cc', e)}
+      />
+    </div>
+
+    <div>
+      <label htmlFor="subject">Subject</label>
+      <input
+        id="subject"
+        type="text"
+        value={subject}
+        onChange={e => onFieldChange('subject', e)}
+      />
+    </div>
+
+    <div>
+      <label htmlFor="body">Body</label>
+      <textarea
+        id="body"
+        value={body}
+        onChange={e => onFieldChange('body', e)}
+      />
+    </div>
+
+    <button type="submit">Send</button>
+  </form>
+)
 
 export default Composer
